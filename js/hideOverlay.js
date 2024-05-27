89fa3ed6e30d049e4f6577155d721be4ffdcc620
Ruben Bensimon <rubensbensimon@Gmail.com>
2024-05-27 21:37:52 +0300

    x

--- js/hideOverlay.js
+++ js/hideOverlay.js
@@ -1,5 +1,5 @@
-function hideOverlay() {
-    var overlay = document.getElementById('overlay');
-    overlay.style.display = "none";
-    document.body.classList.remove("no-scroll");
-}
+// function hideOverlay() {
+//     var overlay = document.getElementById('overlay');
+//     overlay.style.display = "none";
+//     document.body.classList.remove("no-scroll");
+// }
