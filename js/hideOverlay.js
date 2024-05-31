beb45e30be20f09f0e85a64a95a4f37c46794ef7
Ruben Bensimon <rubensbensimon@Gmail.com>
2024-05-31 13:42:41 +0300

    push

--- js/hideOverlay.js
+++ js/hideOverlay.js
@@ -1,4 +1,11 @@
+// function hideOverlay() {
+//     var overlay = document.getElementById('overlay');
+//     overlay.style.display = "none";
+//     document.body.classList.remove("no-scroll");
+// }
+
 function hideOverlay() {
+    console.log("hideOverlay called");
     var overlay = document.getElementById('overlay');
     overlay.style.display = "none";
     document.body.classList.remove("no-scroll");
--- js/playMusic.js
+++ js/playMusic.js
@@ -1,6 +1,18 @@
+// function playMusic() {
+//     var audio = document.getElementById("myAudio");
+//     audio.play().catch(error => {
+//         console.error("Audio play failed:", error);
+//     });
+// }
+
+
 function playMusic() {
+    console.log("playMusic called");
     var audio = document.getElementById("myAudio");
-    audio.play().catch(error => {
+    audio.load(); // Ensure the audio is loaded before playing
+    audio.play().then(() => {
+        console.log("Audio played successfully");
+    }).catch(error => {
         console.error("Audio play failed:", error);
     });
 }
